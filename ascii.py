e61defa628418cf3f84eb350736ba34bf0e1ace2
LuukHoutzager <1041395@hr.nl>
2022-05-19 18:49:43 +0200

    ASCII Init
    V1

---
+++ ascii.py
@@ -0,0 +1,6 @@
+import ascii_magic
+
+""" This code will create ascii image from the chosen image in the image directory. Place the wanted image in the directory and run the file. """
+
+draw_image = ascii_magic.from_image_file("./images/dart.jpg", colums=200, char="#")
+ascii_magic.to_terminal(draw_image)
