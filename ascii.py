3331749d6ed57776c3a3e44bc3acd79e612ae02e
LuukHoutzager <1041395@hr.nl>
2022-05-19 18:51:07 +0200

    Final changes

--- ascii.py
+++ ascii.py
@@ -3,4 +3,7 @@ import ascii_magic
 """ This code will create ascii image from the chosen image in the image directory. Place the wanted image in the directory and run the file. """
 
 draw_image = ascii_magic.from_image_file("./images/dart.jpg", colums=200, char="#")
-ascii_magic.to_terminal(draw_image)
+
+
+if __name__ == "__main__":
+    ascii_magic.to_terminal(draw_image)
